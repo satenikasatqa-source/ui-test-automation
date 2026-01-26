41f6396d07f5725d15665139858ca4d175486ed0
Satenik <satenik.asat.qa@gmail.com>
2026-01-26 21:51:21 +0300

    Configure remote selenoid with default and Jenkins override

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,11 +1,6 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
@@ -15,34 +10,20 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.timeout = 60000;
 
-        String remote = System.getProperty("remote");
-        if (remote != null && !remote.isBlank()) {
-            Configuration.remote = remote;
+        Configuration.remote = System.getProperty(
+                "remote",
+                "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+        );
 
-            MutableCapabilities capabilities = new MutableCapabilities();
-            capabilities.setCapability("selenoid:options",
-                    Map.of("enableVNC", true, "enableVideo", true));
-            Configuration.browserCapabilities = capabilities;
-        }
-    }
-
-    @BeforeEach
-    void addAllureListener() {
-        // на всякий случай, чтобы не копились листенеры
-        SelenideLogger.removeListener("allure");
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-        Selenide.closeWebDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 }
